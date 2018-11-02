b3e0b463394d6675693265d6df307c48f4e7aab5
Sayali Deshmane <sd796@njit.edu>
2018-11-01 17:02:12 -0700

    Fix: created a unit test to count the number of records inserted by the database seed.

---
+++ tests/Unit/CountUsersTest.php
@@ -0,0 +1,14 @@
+<?php
+namespace Tests\Unit;
+use Tests\TestCase;
+use \App\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+class CountUserTest extends TestCase
+{
+    public function testCountUser(){
+        $users=User::all();
+        $users->count();
+        $this->assertEquals(50,count($users),"Should return 50 users");
+    }
+}
